de22d7990e3b7ecacdfa0fa0e0f94fc063c89d47
jmorille <jmorille@gmail.com>
2018-10-20 11:35:24 +0200

    commit lastfile

--- src/dialog/index.js
+++ src/dialog/index.js
@@ -2,17 +2,26 @@
 const {dialogflow} = require('actions-on-google');
 const { BasicCard, Button, Image} = require('actions-on-google');
 
-const app = dialogflow({debug: false});
-
-
 const versionApp = require('./versionApp');
 const jenkinsApp = require('./jenkinsApp');
+const signIn = require('./signIn');
+
+
+const app = dialogflow({
+    debug: true,
+    clientId: "789987504517-i8tdg4f3aeebinqm5scdd6bgqj0o6alk.apps.googleusercontent.com",
+});
+
+
 
 app.intent('app-version',  versionApp.versionApp);
 
 app.intent('app-deploy', jenkinsApp.deployRelease);
 app.intent('app-compile', jenkinsApp.compileApp);
 app.intent('app-release', jenkinsApp.releaseApp);
+
+app.intent('ask_for_sign_in', signIn.ask_for_sign_in);
+app.intent('ask_for_sign_in_confirmation', signIn.ask_for_sign_in_confirmation);
 
 
 
@@ -22,9 +31,6 @@ app.intent('make_name', (conv, {color, number}) => {
 });
 
 
-app.intent('which_language', (conv, {language } )=> {
-    conv.ask("coucou");
-    conv.ask("et une autres question ?");
-});
+
 
 module.exports = app;
--- src/dialog/signIn.js
+++ src/dialog/signIn.js
@@ -1,18 +1,24 @@
+const log = require('../logger');
 const {SignIn} = require('actions-on-google');
 
 module.exports = {
 
     'ask_for_sign_in': (conv) => {
-        conv.ask(new SignIn());
+        log.info("Ask for Sign In !!!!!!!!!!!!!!!!!!");
+        conv.ask(new SignIn("Local Auth"));
+        log.info("Ask for Sign In _____________________");
+        return conv;
     },
 
     'ask_for_sign_in_confirmation': (conv, params, signin) => {
+        log.info("ask_for_sign_in_confirmation !!!!!!!!!!!!!!!!", signin);
         if (signin.status !== 'OK') {
-            return conv.ask('You need to sign in before using the app.');
+            return conv.ask("Vous devez vous identifiez avant d'utiliser l'application.");
         }
         // const access = conv.user.access.token;
-        // possibly do something with access token
-        return conv.ask('Great! Thanks for signing in.');
+        const payload = conv.user.profile.payload;
+        conv.ask(`Bonjour, ${payload.name}. Que voulez vous faire maintenant?`);
+        return conv;
     },
 
 };
